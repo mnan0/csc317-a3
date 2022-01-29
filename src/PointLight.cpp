1a00da0388e5ccec0de6aa7156c427c499b8b190
Peter Sellars <p.sellars@mail.utoronto.ca>
2022-01-29 12:51:45 -0500

    done point light

--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -4,6 +4,10 @@ void PointLight::direction(
   const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
+  
+  // remember to normalize the returned direction vector
+  Eigen::Vector3d reg_direct = p - q;
+  max_t = reg_direct.norm();
+  d = reg_direct.normalized();
   ////////////////////////////////////////////////////////////////////////////
 }
