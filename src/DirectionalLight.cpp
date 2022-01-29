7f1da6ecc7a323bcb2d810888b2808d9ad5a0c42
Peter Sellars <p.sellars@mail.utoronto.ca>
2022-01-29 13:29:25 -0500

    done directional light

--- src/DirectionalLight.cpp
+++ src/DirectionalLight.cpp
@@ -5,7 +5,8 @@ void DirectionalLight::direction(
   const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+  max_t = std::numeric_limits<double>::max();
+  d = q + (-1.0)*(this->d);
   ////////////////////////////////////////////////////////////////////////////
 }
 
